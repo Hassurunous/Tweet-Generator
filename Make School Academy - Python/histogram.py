bf317f8dbb2d8b1d2a2f33f03f5c84738801c89c
Michael Loubier <michael.a.loubier@gmail.com>
2017-01-26 14:01:10 -0800

    Completed first stochastic challenge. Refactored histogram to include an if __name__ = '__main__' so that I could import the function without running the rest of the script.

--- Make School Academy - Python/histogram.py
+++ Make School Academy - Python/histogram.py
@@ -1,15 +1,17 @@
 # Import operator for sorting dictionary at the end.
 import operator
 
-# Create empty dictionary to store words from the document.
-dictionary = {}
+# Import sys for system functions
+import sys
 
 
 # Function takes in a file and splits it into its words.
 # Then function stores those words in dictionary, keeping a count
 # of each word, rather than storing a word multiple times.
-def dictionaryFill(file):
-    with open(file) as f:
+def dictionaryFill(filename):
+    # Create empty dictionary to store words from the document.
+    dictionary = {}
+    with open(filename) as f:
         for line in f:
             for word in line.split():
                 word.lower()
@@ -17,18 +19,24 @@ def dictionaryFill(file):
                     dictionary[word] += 1
                 else:
                     dictionary[word] = 1
-    # Sort the dictionary by how many of each word were present in the file.
-    sorted_dictionary = sorted(dictionary.items(), key=operator.itemgetter(1), reverse=True)
-
-    # Print the sorted dictionary, now a list of tuples.
-    print(sorted_dictionary)
+    return dictionary
 
 
 # # Run the dictionaryFill function
 # dictionaryFill("spacexarticle")
 
-# Ask user for name of file to open.
-f = input("Name of file to open?\n")
+# # Ask user for name of file to open.
+if __name__ == '__main__':
+    f = input("Name of file to open? (Type 'quit' to exit.)\n")
 
-# Run dictionaryFill on that file.
-dictionaryFill(f)
+    # Run dictionaryFill on that file.
+    if f == "quit":
+        sys.exit()
+    else:
+        dictionary = dictionaryFill(f)
+
+    # Sort the dictionary by how many of each word were present in the file.
+    sorted_dictionary = sorted(dictionary.items(), key=operator.itemgetter(1), reverse=True)
+
+    # Print the sorted dictionary, now a list of tuples.
+    print(sorted_dictionary)
---
+++ Make School Academy - Python/stochastic.py
@@ -0,0 +1,24 @@
+# Import out Histogram program
+from histogram import dictionaryFill
+
+# Import random for a random number
+import random
+
+# Import operator for dictionary sort.
+import operator
+
+
+# Build a dictionary from the function "dictionaryFill(filename)"" in
+# histogram.py and sort those values for indexing.
+# Ask user for name of file to open.
+f = input("Name of file to open? (Type 'quit' to exit.)\n")
+
+# Run dictionaryFill on that file.
+if f == "quit":
+    sys.exit()
+else:
+    dictionary = dictionaryFill(f)
+
+sorted_dictionary = sorted(dictionary.items(), key=operator.itemgetter(1), reverse=True)
+
+print(sorted_dictionary[random.randrange(0, len(dictionary))])
