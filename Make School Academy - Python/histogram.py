673f907a5e1c57969c8b2a2af7201506937f404e
Michael Loubier <michael.a.loubier@gmail.com>
2017-02-06 20:21:14 -0800

    Completed second stochastic example and pushing flask example to heroku.

--- Make School Academy - Python/histogram.py
+++ Make School Academy - Python/histogram.py
@@ -14,16 +14,37 @@ import string
 def dictionaryFill(filename):
     # Create empty dictionary to store words from the document.
     dictionary = {}
+    # Translator holds the argument information we will need to remove
+    # punctuation from the words when we use .translate
+    translator = str.maketrans('', '', string.punctuation)
+    # Use 'with' to also close file when the function is complete
     with open(filename) as f:
-        translator = str.maketrans('', '', string.punctuation)
+        # Split the file into its lines
         for line in f:
+            # Split the line into its words
             for word in line.split():
+                # Take the word, remove the punctuation, and lowercase it
                 word = word.translate(translator).lower()
+                # If the word is already in dictionary, then increment,
+                # otherwise add it to the dictionary
                 if word in dictionary:
                     dictionary[word] += 1
                 else:
                     dictionary[word] = 1
+    # Return the full dictionary
     return dictionary
+
+
+def uniqueWords(histogram):
+    uniqueWords = 0
+    for key in histogram:
+        uniqueWords += 1
+    return uniqueWords
+
+
+def frequency(filename, word):
+    dictionary = dictionaryFill(filename)
+    return dictionary[word] if word in dictionary else 0
 
 
 # # Run the dictionaryFill function
@@ -31,16 +52,40 @@ def dictionaryFill(filename):
 
 # # Ask user for name of file to open.
 if __name__ == '__main__':
-    f = input("Name of file to open? (Type 'quit' to exit.)\n")
+    f = input("BUILD a dictionary from a file, COUNT words in the file, or find FREQUENCY of a word? (Type 'quit' at any time to exit.)\n")
 
     # Run dictionaryFill on that file.
-    if f == "quit":
-        sys.exit()
+    if type(f) == str:
+        if f == "quit":
+            sys.exit()
+        elif f.lower() == "build":
+            f = input("What is the name of the file you wish to open?\n")
+            if f == "quit":
+                sys.exit()
+            else:
+                print(dictionaryFill(f))
+        elif f.lower() == "count":
+            f = input("What is the name of the file you wish to open?\n")
+            if f == "quit":
+                sys.exit()
+            else:
+                print("There are", uniqueWords(dictionaryFill(f)), "words in this file.")
+        elif f.lower() == "frequency":
+            f = input("What is the name of the file you wish to open?\n")
+            if f == "quit":
+                sys.exit()
+            else:
+                g = input("What word are you looking for in the file?\n")
+                if g == "quit":
+                    sys.exit()
+                else:
+                    print("The word", g, "shows up", frequency(f, g), "times in the file.")
     else:
-        dictionary = dictionaryFill(f)
+        print("Must input a file name as string.")
+        sys.exit()
 
-    # Sort the dictionary by how many of each word were present in the file.
-    sorted_dictionary = sorted(dictionary.items(), key=operator.itemgetter(1), reverse=True)
-
-    # Print the sorted dictionary, now a list of tuples.
-    print(sorted_dictionary)
+    # # Sort the dictionary by how many of each word were present in the file.
+    # sorted_dictionary = sorted(dictionary.items(), key=operator.itemgetter(1), reverse=True)
+    #
+    # # Print the sorted dictionary, now a list of tuples.
+    # print(sorted_dictionary)
--- Make School Academy - Python/stochastic.py
+++ Make School Academy - Python/stochastic.py
@@ -10,6 +10,9 @@ import operator
 # Import sys to exit if 'quit' given
 import sys
 
+# Import timeit to test code runtime
+from timeit import Timer
+
 
 # This function returns a random key from the dictionary of words
 # Dictionary is filled based on the input file
@@ -21,13 +24,37 @@ def dictionaryRandom(filename):
 # This function returns a random key from the dictionary, weighted by the
 # number of times the word appears in the seeded file.
 # Dictionary is filled based on the input file.
-def dictionaryRandomWeighted(filename):
-    dictionary = dictionaryFill(filename)
+# Inefficient function creates an array. Refactoring for efficiency.
+def dictionaryRandomArray(dictionary):
     weighted_dictionary = []
     for key, value in dictionary.items():
         for item in range(value):
             weighted_dictionary.append(key)
     return weighted_dictionary[random.randrange(0, len(weighted_dictionary)-1)]
+
+
+def dictionaryRandomWeighted(dictionary):
+    # Create an indexed list of tuples from the dictionary
+    word_list = list(dictionary.items())
+    # Find the total number of words in the dictionary
+    word_range = sum(dictionary.values())
+    target = random.randrange(0, word_range - 1)
+    # Count and Index used to iterate through list to find word matching
+    # the target number, based on the values. Much like a number line.
+    count = 0
+    index = 0
+    # Iterate through the list of words until you reach the target number
+    # and increment your counter based on the value of each key in the
+    # dictionary, now a list of tuples
+    while(count < target):
+        # Add the value of the current word to the ongoing counter
+        count += word_list[index][1]
+        # If the value of count is still below the target, increment index
+        if(count < target):
+            index += 1
+    else:
+        # When the loop finds the word, return the word
+        return(word_list[index][0])
 
 
 # This function sorts a dictionary based on the values of each key
@@ -54,12 +81,37 @@ if __name__ == '__main__':
             # Print an error message
             print("Function dictionaryRandom requires a string input.")
     elif f.lower() == "weighted" or f.lower() == "weighted random":
-        # Ask user for file name
-        f = input("What is the name of the file? (Type 'quit' to exit.)\n")
-        # Check to make sure that the user has input a string
-        if type(f) == str:
-            # Print word returned by dictionaryRandomWeighted()
-            print(dictionaryRandomWeighted(f))
+        f = input("Random word by converting to ARRAY or from word WEIGHT?( Type 'quit' to exit.)\n")
+        if f.lower() == "array" or f.lower() == "converting to array":
+            # Ask user for file name
+            f = input("What is the name of the file? (Type 'quit' to exit.)\n")
+            # Check to make sure that the user has input a string
+            if type(f) == str:
+                # Print word returned by dictionaryRandomWeighted()
+                dictionary = dictionaryFill(f)
+                t = Timer("""dictionaryRandomArray(dictionary)""", setup="from __main__ import dictionaryRandomArray, dictionary")
+                print("Run time from 100 runs =", t.timeit(100))
+                print("Your word is: ", dictionaryRandomArray(dictionary))
+            else:
+                # Error message or quit command
+                print("'Quit' or invalid input detected. Quitting program...")
+                sys.exit()
+        if f.lower() == "weight" or f.lower() == "word weight":
+            # Ask user for file name
+            f = input("What is the name of the file? (Type 'quit' to exit.)\n")
+            # Check to make sure that the user has input a string
+            if type(f) == str:
+                # Fill dictionary with words from file given
+                dictionary = dictionaryFill(f)
+                # Print word returned by dictionaryRandomWeighted()
+                t = Timer("""dictionaryRandomWeighted(dictionary)""", setup="from __main__ import dictionaryRandomWeighted, dictionary")
+                print("Run time from 100 runs =", t.timeit(100))
+                print("Your word is:", dictionaryRandomWeighted(dictionary))
+            else:
+                # Error message or quit command
+                print("'Quit' or invalid input detected. Quitting program...")
+                sys.exit()
         else:
-            # Print an error message.
-            print("Function dictionaryRandomWeighted requires a string input.")
+            # Error message or quit command
+            print("'Quit' or invalid input detected. Quitting program...")
+            sys.exit()
